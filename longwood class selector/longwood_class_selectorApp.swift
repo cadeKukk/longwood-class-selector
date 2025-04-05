2db089f312727885861f90365f9c6708682c671f
cade <cadekukk@gmail.com>
2025-04-05 13:59:22 -0400

    Initial Commit

---
+++ longwood class selector/longwood_class_selectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  longwood_class_selectorApp.swift
+//  longwood class selector
+//
+//  Created by Cade Kukk on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct longwood_class_selectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ longwood class selectorTests/longwood_class_selectorTests.swift
@@ -0,0 +1,17 @@
+//
+//  longwood_class_selectorTests.swift
+//  longwood class selectorTests
+//
+//  Created by Cade Kukk on 4/5/25.
+//
+
+import Testing
+@testable import longwood_class_selector
+
+struct longwood_class_selectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
